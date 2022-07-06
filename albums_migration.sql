082af1d6d1a2ab76399df2025348c48deb6989b0
Michael Friend <michael.l.friend2@gmail.com>
2022-07-06 11:16:29 -0500

    updated

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,10 +2,10 @@ USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (30) NOT NULL,
-    name VARCHAR (30) NOT NULL,
+    artist VARCHAR (50) NOT NULL,
+    name VARCHAR (100) NOT NULL,
     release_date INT (4) NOT NULL,
     sales FLOAT UNSIGNED NOT NULL,
-    genre VARCHAR (30) NOT NULL,
+    genre VARCHAR (50) NOT NULL,
     PRIMARY KEY (id)
 );
