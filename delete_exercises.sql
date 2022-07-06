8574d58d6a640938298cd1b171e0b9fcd14397fb
Michael Friend <michael.l.friend2@gmail.com>
2022-07-06 12:10:01 -0500

    completed

---
+++ delete_exercises.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+SELECT 'Albums released after 1991' AS 'Selected';
+SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+SELECT 'Albums with the genre of Disco' AS 'Selected';
+SELECT * FROM albums WHERE genre = 'Disco';
+DELETE FROM albums WHERE genre = 'Disco';
+SELECT 'Albums by Whitney Houston' AS 'Selected';
+SELECT * FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist = 'Whitney Houston';
