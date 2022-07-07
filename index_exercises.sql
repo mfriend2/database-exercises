f3e1b3c7837026c5916945f5b5e0fa5d7871ada2
Michael Friend <michael.l.friend2@gmail.com>
2022-07-07 15:04:05 -0500

    completed

---
+++ index_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+DESCRIBE titles;
+DESCRIBE employees;
+DESCRIBE departments;
+DESCRIBE dept_emp;
+DESCRIBE dept_manager;
+DESCRIBE salaries;
+USE codeup_test_db;
+ALTER TABLE albums ADD UNIQUE (name, artist);
